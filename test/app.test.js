4cd18fbf1ad55361fe4cee442386cd9aef5d58c5
Daniel <dsotillo20@gmail.com>
2026-02-12 15:34:01 -0500

    test: add multiplication test case to app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,9 @@
-const suma = require('../app');
+const {suma, mult} = require('../app');
 
 test('suma 2 + 3 = 5', () => {
     expect(suma(2,3)).toBe(5);
+});
+
+test('mutl 5 * 4 = 20', () => {
+    expect(mult(5,4)).toBe(5);
 });
